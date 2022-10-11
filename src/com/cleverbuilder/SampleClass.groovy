3e97c219a6ed44bdf18307f13b8669b7f5d8941a
Jagadish Vemugunta <jvemugunta@corelogic.com>
2022-10-11 12:14:09 -0400

    added code

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -8,4 +8,17 @@ class SampleClass {
    def increaseAge(Integer years) {
       this.age += years
    }
+
+   def String callGet()
+   {
+        // GET
+      def get = new URL("https://httpbin.org/get").openConnection();
+      def getRC = get.getResponseCode();
+      println(getRC);
+      if(getRC.equals(200)) {
+         println(get.getInputStream().getText());
+      }
+   }
 }
+
+ 
